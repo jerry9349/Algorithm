7a545e1d79b50f525790aa497ef3463ae646c5a0
HaeLeeJi <38179990+jerry9349@users.noreply.github.com>
2022-08-23 09:06:08 +0900

    배열
    파이썬 배열 연습

---
+++ algorithm.py
@@ -0,0 +1,25 @@
+n = 10
+a = [0] * n
+print(a)
+
+array = [i for i in range(10)]
+print(array)
+
+b = [1,2,3,4]
+b.insert(2,5)
+print(b)
+
+c = [1,2,3,4,5,5,5]
+result_set = {3,5}
+result = [i for i in result_set]
+print(result)
+# 출력 결과 : [3, 5]
+result = [i for i in c if result_set]
+print(result)
+# 출력 결과 : [1, 2, 3, 4, 5, 5, 5]
+result = [i for i in c if i in result_set]
+print(result)
+# 출력 결과 : [3, 5, 5, 5]
+result = [i for i in c if i not in result_set]
+print(result)
+# 출력 결과 : [1, 2, 4]
