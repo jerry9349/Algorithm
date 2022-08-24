524e51cd4655e39e91db0f63449d0c9549497550
HaeLeeJi <38179990+jerry9349@users.noreply.github.com>
2022-08-24 11:00:14 +0900

    반복문
    파이썬 반복문 연습

--- algorithm/20220818.py
+++ algorithm/20220818.py
@@ -9,6 +9,8 @@ elif a>=-10:
   print("0 > a >= -10")
 else:
   print("-10 > a")
+# 출력 결과 : a >= 0
+
 
 # X가 모두 참인지 체크 : X and Y
 # X, Y 둘중 하나만 참인지 체크 : X or Y
@@ -16,10 +18,11 @@ else:
 
 if True or False:
   print("Yes")
-
+# 출력 결과 : Yes
 b = 15
 if b <= 20 and b >=0:
   print("Yes")
+# 출력 결과 : Yes
 
 
 # 리스트, 튜플, 문자열, 딕셔너리 모두 사용 가능한 in 연산자와 not in 연산자
@@ -32,17 +35,21 @@ if c >= 30:
   pass
 else :
   print("a < 30")
+# 출력 결과 : 없음
+
 
 # 조건문 간소화
 # 조건문에서 실행될 소스코드가 한줄인 경우, 줄바꿈 필요없음
 # if ~ else 문을 한줄에 작성
 score = 85
+# 1)
 if score >= 80 :result="success"
 else : result = "fail"
-
+# 2)
 result = "success" if score >= 80 else "fail"
 
 print(result)
+# 출력 결과 : success
 
 
 # 조건문 내에서 부등식
@@ -51,5 +58,5 @@ x = 15
 if 0 < x < 20:
   print("x는 0 이상 20 미만의 수 입니다.")
 
-
+# 출력 결과 : x는 0 이상 20 미만의 수 입니다.
 
